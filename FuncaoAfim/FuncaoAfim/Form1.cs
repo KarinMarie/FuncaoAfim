b17810f0e309f18756c38d66ce9f92850bc81d44
Karin Marie <93265472+KarinMarie@users.noreply.github.com>
2022-04-13 13:51:17 -0300

    Add files via upload

---
+++ FuncaoAfim/FuncaoAfim/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuncaoAfim
+{
+    public partial class Form1 : Form
+    {
+        FuncaoAfim funcao;
+        DataBaseManager banco;
+
+        public Form1()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US"); // Evitar um problema que eu tava tendo com o número decimal passando vírgulas para a consulta SQL.
+            InitializeComponent();
+            funcao = new FuncaoAfim(numA.Value, numB.Value, chartFuncao);
+            banco = new DataBaseManager("FuncaoAfim");
+            chartFuncao = funcao.GerarGrafico();
+            lbFuncao.Text = funcao.FuncaoTexto();
+            CarregarHistorico();
+        }
+
+        private void btnGerarGrafico_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                funcao = new FuncaoAfim(numA.Value, numB.Value, chartFuncao);
+                chartFuncao = funcao.GerarGrafico();
+                lbFuncao.Text = funcao.FuncaoTexto();
+
+                banco.AtualizarBanco($"INSERT INTO Funcoes VALUES({numA.Value}, {numB.Value}, '{lbFuncao.Text}')");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CarregarHistorico()
+        {
+            listFuncoes.Items.Clear();
+
+            DataTable tabela = banco.ConsultarBanco("SELECT Funcao FROM Funcoes ORDER BY ID DESC");
+
+            if(tabela != null)
+            {
+                for(int i = 0; i < tabela.Rows.Count; i++)
+                {
+                    string linha = tabela.Rows[i]["Funcao"].ToString();
+                    listFuncoes.Items.Add(linha);
+                }
+            }
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CarregarHistorico();
+        }
+
+        private void btnLimparHistorico_Click(object sender, EventArgs e)
+        {
+            banco.ConsultarBanco("TRUNCATE TABLE Funcoes");
+            listFuncoes.Items.Clear();
+        }
+
+        private void listFuncoes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable tabela = banco.ConsultarBanco($"SELECT * FROM Funcoes WHERE Funcao = '{listFuncoes.SelectedItem}'");
+
+            if (tabela != null)
+            {
+                if (listFuncoes.SelectedIndex != -1)
+                {
+                    decimal A = Convert.ToDecimal(tabela.Rows[0]["A"]);
+                    decimal B = Convert.ToDecimal(tabela.Rows[0]["B"]);
+
+                    funcao = new FuncaoAfim(A, B, chartFuncao);
+                    chartFuncao = funcao.GerarGrafico();
+                    lbFuncao.Text = funcao.FuncaoTexto();
+
+                    numA.Value = A;
+                    numB.Value = B;
+
+                    tabControl1.SelectedIndex = 0;
+                }
+                else
+                    MessageBox.Show("Por favor, selecione um item da lista.", "ATENÇÃO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+        }
+    }
+}
+
+// https://coolors.co/d8e2dc-ffe5d9-ffcad4-f4acb7-9d8189
+// https://icoconvert.com/
---
+++ FuncaoAfim/FuncaoAfim/FuncaoAfim.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FuncaoAfim
+{
+    class FuncaoAfim
+    {
+        private decimal A;
+        private decimal B;
+        private Chart grafico;
+
+        public FuncaoAfim(decimal A, decimal B, Chart grafico)
+        {
+            this.A = A;
+            this.B = B;
+            this.grafico = grafico;
+        }
+
+        private decimal CalcularPontoY()
+        {
+            return B;
+        }
+
+        private decimal CalcularPontoX()
+        {
+            return (0 - B) / A;
+        }
+
+        public Chart GerarGrafico()
+        {
+            
+            // DEFINIR OS QUADRANTES DO GRÁFICO:
+            grafico.ChartAreas[0].AxisX.Maximum = 10;
+            grafico.ChartAreas[0].AxisX.Minimum = -10;
+           
+            grafico.ChartAreas[0].AxisY.Maximum = 10;
+            grafico.ChartAreas[0].AxisY.Minimum = -10;
+            // ------------------------------------------
+
+            // DEFININDO OS EIXOS X E Y DO GRÁFICO:
+            grafico.ChartAreas[0].AxisX.Crossing = 0;
+            grafico.ChartAreas[0].AxisY.Crossing = 0;
+            // ------------------------------------------
+
+            // RETIRANDO AS GRADES DO GRÁFICO:
+            grafico.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            grafico.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            grafico.ChartAreas[0].AxisX.MinorGrid.Enabled = false;
+            grafico.ChartAreas[0].AxisY.MinorGrid.Enabled = false;
+            // ------------------------------------------
+
+            // LIMPANDO OS PONTOS ANTERIORES E
+            // ADICIONANDO OS PONTOS QUE CORTAM OS EIXOS X E Y:
+            grafico.Series[0].Points.Clear();
+            grafico.Series[0].Points.AddXY(0, CalcularPontoY());
+            grafico.Series[0].Points.AddXY(CalcularPontoX(), 0);
+            // ------------------------------------------
+
+            // ADICIONANDO OUTROS PONTOS PARA TORNAR A LINHA DO GRÁFICO MAIS LONGA:
+            decimal Y;
+            for (int X = -5; X <= 10; X++)
+            {
+                Y = A * X + B;
+                grafico.Series[0].Points.AddXY(X, Y);
+            }
+            // ------------------------------------------
+
+            grafico.Series[0].Color = Color.FromArgb(229, 56, 82); // Muda a cor da linha do gráfico
+            return grafico;
+        }
+
+        public string FuncaoTexto()
+        {
+            if (B >= 0)
+                return $"f(x) = {A}x + {B}";
+            else
+                return $"f(x) = {A}x - {B*-1}";
+        }
+    }
+}
