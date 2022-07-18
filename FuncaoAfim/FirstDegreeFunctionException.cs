c74e9625fd6c225b320f14bb432ecf22ceb67a33
Karin Marie <93265472+KarinMarie@users.noreply.github.com>
2022-07-18 20:31:32 -0300

    Add files via upload

---
+++ FuncaoAfim/FirstDegreeFunctionException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoAfim
+{
+    class FirstDegreeFunctionException : Exception
+    {
+        public override string Message => "Em uma função linear, o A não pode ser igual a zero!";
+    }
+}
---
+++ FuncaoAfim/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace FuncaoAfim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chartFuncao = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbFuncao = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numA = new System.Windows.Forms.NumericUpDown();
+            this.numB = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGerarGrafico = new System.Windows.Forms.Button();
+            this.listFuncoes = new System.Windows.Forms.ListBox();
+            this.btnLimparHistorico = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartFuncao)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 59);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(380, 578);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(229)))), ((int)(((byte)(217)))));
+            this.tabPage1.Controls.Add(this.btnGerarGrafico);
+            this.tabPage1.Controls.Add(this.numB);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.numA);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.lbFuncao);
+            this.tabPage1.Controls.Add(this.chartFuncao);
+            this.tabPage1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.tabPage1.Location = new System.Drawing.Point(4, 33);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(372, 541);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Principal";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(229)))), ((int)(((byte)(217)))));
+            this.tabPage2.Controls.Add(this.btnLimparHistorico);
+            this.tabPage2.Controls.Add(this.listFuncoes);
+            this.tabPage2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.tabPage2.Location = new System.Drawing.Point(4, 33);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(372, 541);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Histórico";
+            // 
+            // chartFuncao
+            // 
+            this.chartFuncao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(229)))), ((int)(((byte)(217)))));
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(229)))), ((int)(((byte)(217)))));
+            chartArea1.Name = "ChartArea1";
+            this.chartFuncao.ChartAreas.Add(chartArea1);
+            this.chartFuncao.Location = new System.Drawing.Point(8, 54);
+            this.chartFuncao.Name = "chartFuncao";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.chartFuncao.Series.Add(series1);
+            this.chartFuncao.Size = new System.Drawing.Size(358, 349);
+            this.chartFuncao.TabIndex = 0;
+            this.chartFuncao.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(56)))), ((int)(((byte)(82)))));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gerador de Função Afim";
+            // 
+            // lbFuncao
+            // 
+            this.lbFuncao.AutoSize = true;
+            this.lbFuncao.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFuncao.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.lbFuncao.Location = new System.Drawing.Point(55, 22);
+            this.lbFuncao.Name = "lbFuncao";
+            this.lbFuncao.Size = new System.Drawing.Size(149, 29);
+            this.lbFuncao.TabIndex = 1;
+            this.lbFuncao.Text = "f(x) = ax + b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.label3.Location = new System.Drawing.Point(44, 433);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "A:";
+            // 
+            // numA
+            // 
+            this.numA.DecimalPlaces = 2;
+            this.numA.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.numA.Location = new System.Drawing.Point(78, 431);
+            this.numA.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numA.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
+            this.numA.Name = "numA";
+            this.numA.Size = new System.Drawing.Size(79, 29);
+            this.numA.TabIndex = 3;
+            this.numA.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // numB
+            // 
+            this.numB.DecimalPlaces = 2;
+            this.numB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.numB.Location = new System.Drawing.Point(244, 431);
+            this.numB.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numB.Minimum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            -2147483648});
+            this.numB.Name = "numB";
+            this.numB.Size = new System.Drawing.Size(79, 29);
+            this.numB.TabIndex = 5;
+            this.numB.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.label4.Location = new System.Drawing.Point(210, 433);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 24);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "B:";
+            // 
+            // btnGerarGrafico
+            // 
+            this.btnGerarGrafico.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(226)))), ((int)(((byte)(220)))));
+            this.btnGerarGrafico.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.btnGerarGrafico.Location = new System.Drawing.Point(6, 484);
+            this.btnGerarGrafico.Name = "btnGerarGrafico";
+            this.btnGerarGrafico.Size = new System.Drawing.Size(360, 51);
+            this.btnGerarGrafico.TabIndex = 6;
+            this.btnGerarGrafico.Text = "Gerar gráfico";
+            this.btnGerarGrafico.UseVisualStyleBackColor = false;
+            this.btnGerarGrafico.Click += new System.EventHandler(this.btnGerarGrafico_Click);
+            // 
+            // listFuncoes
+            // 
+            this.listFuncoes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(235)))), ((int)(((byte)(238)))));
+            this.listFuncoes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(129)))), ((int)(((byte)(137)))));
+            this.listFuncoes.FormattingEnabled = true;
+            this.listFuncoes.ItemHeight = 24;
+            this.listFuncoes.Location = new System.Drawing.Point(6, 18);
+            this.listFuncoes.Name = "listFuncoes";
+            this.listFuncoes.Size = new System.Drawing.Size(360, 460);
+            this.listFuncoes.TabIndex = 0;
+            this.listFuncoes.SelectedIndexChanged += new System.EventHandler(this.listFuncoes_SelectedIndexChanged);
+            // 
+            // btnLimparHistorico
+            // 
+            this.btnLimparHistorico.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(226)))), ((int)(((byte)(220)))));
+            this.btnLimparHistorico.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.btnLimparHistorico.Location = new System.Drawing.Point(6, 484);
+            this.btnLimparHistorico.Name = "btnLimparHistorico";
+            this.btnLimparHistorico.Size = new System.Drawing.Size(360, 51);
+            this.btnLimparHistorico.TabIndex = 2;
+            this.btnLimparHistorico.Text = "Limpar Histórico";
+            this.btnLimparHistorico.UseVisualStyleBackColor = false;
+            this.btnLimparHistorico.Click += new System.EventHandler(this.btnLimparHistorico_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.label2.Location = new System.Drawing.Point(3, 636);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Karin Marie, 2022.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(202)))), ((int)(((byte)(212)))));
+            this.ClientSize = new System.Drawing.Size(382, 655);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gerador de Função do 1º Grau";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartFuncao)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnGerarGrafico;
+        private System.Windows.Forms.NumericUpDown numB;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numA;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbFuncao;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartFuncao;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listFuncoes;
+        private System.Windows.Forms.Button btnLimparHistorico;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
